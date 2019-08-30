54cc693735c73874d83f24206b6592717a6705c4
MityaDementiy <d.dementiy@gmail.com>
2019-08-30 07:48:22 +0300

    Refactoring tests — move expected result to fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,13 @@
 import path from 'path';
+import fs from 'fs';
 import genDiff from '../src';
+
 
 const testDir = './__tests__/__fixtures__';
 const beforeJson = path.resolve(testDir, 'before.json');
 const afterJson = path.resolve(testDir, 'after.json');
-const expectedResultFlat = `{
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
-  + timeout: 20
-  - timeout: 50
-    host: hexlet.io
-}`;
+const expectedResultFlatJson = fs.readFileSync(path.resolve(testDir, 'expectedFlatJson'), 'utf-8');
 
 test('genDiff', () => {
-  expect(genDiff(beforeJson, afterJson)).toEqual(expectedResultFlat);
+  expect(genDiff(beforeJson, afterJson)).toEqual(expectedResultFlatJson);
 });

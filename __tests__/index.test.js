d1d7310f1e25e8719e1a2275c60b279461fbe313
MityaDementiy <d.dementiy@gmail.com>
2019-10-19 19:30:02 +0300

    Refactoring tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,6 +4,9 @@ import genDiff from '../src';
 const testDir = './__tests__/__fixtures__';
 const expectedFlat = fs.readFileSync(`${testDir}/expectedFlat`, 'utf-8');
 const expectedNested = fs.readFileSync(`${testDir}/expectedNested`, 'utf-8');
+const expectedFlatPlain = fs.readFileSync(`${testDir}/expectedFlatPlain`, 'utf-8');
+const expectedNestedPlain = fs.readFileSync(`${testDir}/expectedPlain`, 'utf-8');
+const expectedNestedJson = fs.readFileSync(`${testDir}/expectedJson`, 'utf-8');
 const flatJsonConfigs = [`${testDir}/before.json`, `${testDir}/after.json`];
 const flatYmlConfigs = [`${testDir}/before.yml`, `${testDir}/after.yml`];
 const flatIniConfigs = [`${testDir}/before.ini`, `${testDir}/after.ini`];
@@ -11,25 +14,18 @@ const nestedJsonConfigs = [`${testDir}/beforeNested.json`, `${testDir}/afterNest
 const nestedYmlConfigs = [`${testDir}/beforeNested.yml`, `${testDir}/afterNested.yml`];
 const nestedIniConfigs = [`${testDir}/beforeNested.ini`, `${testDir}/afterNested.ini`];
 
-const configsToTestDefaultOutput = [
+const configsToTest = [
   [...flatJsonConfigs, expectedFlat],
   [...flatYmlConfigs, expectedFlat],
   [...flatIniConfigs, expectedFlat],
   [...nestedJsonConfigs, expectedNested],
   [...nestedYmlConfigs, expectedNested],
   [...nestedIniConfigs, expectedNested],
+  [...flatJsonConfigs, expectedFlatPlain, 'plain'],
+  [...nestedJsonConfigs, expectedNestedPlain, 'plain'],
+  [...nestedJsonConfigs, expectedNestedJson, 'json'],
 ];
 
-const configsToTestFormattedOutput = [
-  [...flatJsonConfigs, fs.readFileSync(`${testDir}/expectedFlatPlain`, 'utf-8'), 'plain'],
-  [...nestedJsonConfigs, fs.readFileSync(`${testDir}/expectedPlain`, 'utf-8'), 'plain'],
-  [...nestedJsonConfigs, fs.readFileSync(`${testDir}/expectedJson`, 'utf-8'), 'json'],
-];
-
-test.each(configsToTestDefaultOutput)('test genDiff with default output', (beforeFileContent, afterFileContent, expected) => {
-  expect(genDiff(beforeFileContent, afterFileContent)).toEqual(expected);
-});
-
-test.each(configsToTestFormattedOutput)('test genDiff with formatted output', (beforeFileContent, afterFileContent, expected, format) => {
+test.each(configsToTest)('test genDiff', (beforeFileContent, afterFileContent, expected, format = 'default') => {
   expect(genDiff(beforeFileContent, afterFileContent, format)).toEqual(expected);
 });
